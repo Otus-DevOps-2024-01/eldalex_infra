f85303bf8d4edacd2d0209acc741f1e10111cf0b
Alexey <leventcov.a.v@gmail.com>
2024-04-04 22:48:04 +0300

    hw7 fix structure

--- terraform/modules/app/variables.tf
+++ terraform/modules/app/variables.tf
@@ -1,12 +1,16 @@
 variable public_key_path {
+  type        = string
   description = "Path to the public key used for ssh access"
 }
 variable app_disk_image {
     description = "Disk image for reddit app"
+    default     = "reddit-base-ruby-1712252244"
 }
 variable subnet_id {
+  type        = string
   description = "Subnet"
 }
 variable private_key_path {
+  type        = string
   description = "Path to the private key used for ssh access"
 }
