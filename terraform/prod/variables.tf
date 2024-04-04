5c17c0ca2a63c089c4e880bd042b123fa78c0ab0
Alexey <leventcov.a.v@gmail.com>
2024-04-04 22:24:29 +0300

    hw7

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -6,11 +6,9 @@ variable "folder_id" {
 }
 variable "zone" {
   description = "Zone"
-  # Значение по умолчанию
-  default = "ru-central1-a"
+  default     = "ru-central1-a"
 }
 variable "public_key_path" {
-  # Описание переменной
   description = "Path to the public key used for ssh access"
 }
 variable "image_id" {
@@ -24,4 +22,10 @@ variable "service_account_key_file" {
 }
 variable "private_key_path" {
   description = "Path to the private key used for ssh access"
+}
+variable "app_disk_image" {
+  description = "Disk image for reddit app"
+}
+variable "db_disk_image" {
+  description = "Disk image for mongodb"
 }
